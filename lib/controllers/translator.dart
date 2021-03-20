cdc752a5dea01e7ed2c97c7f3052d44320b203de
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-03-20 22:50:25 +0100

    Translate to Morse + translator

--- lib/controllers/translator.dart
+++ lib/controllers/translator.dart
@@ -1,88 +1,11 @@
 import 'package:morse_code_translator/models/alphabet.dart';
 
+//https://en.wikipedia.org/wiki/Morse_code
+
 class Translator {
   //https://stackoverflow.com/questions/50431055/what-is-the-difference-between-the-const-and-final-keywords-in-dart
   static const _letter_gap = '   ';
   static const _word_gap = '       ';
-  static const _alphabetInternationalITU = {
-    'a' : '.-',
-    'b' : '-...',
-    'c' : '-.-.',
-    'd' : '-..',
-    'e' : '.',
-    'f' : '..-.',
-    'g' : '--.',
-    'h' : '....',
-    'i' : '..',
-    'j' : '.---',
-    'k' : '-.-',
-    'l' : '.-..',
-    'm' : '--',
-    'n' : '-.',
-    'o' : '---',
-    'p' : '.--.',
-    'q' : '--.-',
-    'r' : '.-.',
-    's' : '...',
-    't' : '-',
-    'u' : '..-',
-    'v' : '...-',
-    'w' : '.--',
-    'x' : '-..-',
-    'y' : '-.--',
-    'z' : '--..',
-    '1' : '.----',
-    '2' : '..---',
-    '3' : '...--',
-    '4' : '....-',
-    '5' : '.....',
-    '6' : '-....',
-    '7' : '--...',
-    '8' : '---..',
-    '9' : '----.',
-    '0' : '-----',
-  };
-  static const _alphabetContinentalGerke = {
-
-  };
-  static const _alphabetAmericanOriginal = {
-    'a' : '.-',
-    'b' : '-...',
-    'c' : '.. .',
-    'd' : '-..',
-    'e' : '.',
-    'f' : '.-.',
-    'g' : '--.',
-    'h' : '....',
-    'i' : '..',
-    'j' : '-.-.',
-    'k' : '-.-',
-    'l' : '-.', //SPECIAL, tone lasts for the full time, no gap
-    'm' : '--',
-    'n' : '-.',
-    'o' : '..',
-    'p' : '.....',
-    'q' : '..-.',
-    'r' : '. ..',
-    's' : '...',
-    't' : '-',
-    'u' : '..-',
-    'v' : '...-',
-    'w' : '.--',
-    'x' : '.-..',
-    'y' : '.. ..',
-    'z' : '... .',
-    '1' : '.--..',
-    '2' : '..-..',
-    '3' : '...-.',
-    '4' : '....-',
-    '5' : '---',
-    '6' : '......',
-    '7' : '--..',
-    '8' : '-....',
-    '9' : '-..-',
-    '0' : '-----',
-  };
 
   static String translateToMorse(String textToTranslate, Alphabet alphabet){
     String morseString = '';
@@ -93,6 +16,7 @@ class Translator {
       } else if (textToTranslate[i] == ' ' || textToTranslate[i] == '.') {
         morseString += _word_gap;
       } else {
+        print("returning null...");
         return null;
       }
     }
@@ -106,9 +30,9 @@ class Translator {
       if(textToTranslate[i] == ' ' || textToTranslate[i] == '.'){
         alphaNumericString += _word_gap;
       }
-      else if(Alphabet.alphabet.containsKey(textToTranslate[i].toLowerCase())){ //Better to have last? Most characters will NOT be punctuation,
+      else if(alphabet.alphabet.containsKey(textToTranslate[i].toLowerCase())){ //Better to have last? Most characters will NOT be punctuation,
         //charToAdd = _alphabet_international_ITU[textToTranslate[i]];
-        alphaNumericString += Alphabet.alphabet[textToTranslate[i].toLowerCase()] + _letter_gap;
+        alphaNumericString += alphabet.alphabet[textToTranslate[i].toLowerCase()] + _letter_gap;
       }
       else {
         return null;
--- lib/widgets/translate_to.dart
+++ lib/widgets/translate_to.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:morse_code_translator/controllers/translator.dart';
 import 'package:morse_code_translator/models/alphabet.dart';
@@ -17,6 +18,8 @@ class _TranslateToMorsePageState extends State<TranslateToMorsePage> {
   String _textToTranslate = '';
   String _textTranslated = '';
 
+  final FilteringTextInputFormatter morseCodeFilter = FilteringTextInputFormatter.allow(RegExp("[a-z A-Z 0-9]"));
+
   @override
   Widget build(BuildContext context) {
     print(isSelected);
@@ -31,7 +34,6 @@ class _TranslateToMorsePageState extends State<TranslateToMorsePage> {
                 key: _formKey,
                 child: Column(
                     children: <Widget>[
-                      // Add TextFormFields and ElevatedButton here.
                       ToggleButtons(
                         children: <Widget>[
                           Icon(Icons.lightbulb),
@@ -49,6 +51,7 @@ class _TranslateToMorsePageState extends State<TranslateToMorsePage> {
                         decoration: InputDecoration(
                             labelText: 'Enter the text to translate'
                         ),
+                        inputFormatters: [morseCodeFilter],
                         validator: (value) {
                           if (value.isEmpty) {
                             return 'Please enter some text';
@@ -62,39 +65,40 @@ class _TranslateToMorsePageState extends State<TranslateToMorsePage> {
                           });
                         },
                       ),
-                      Row(
-                        children: [],
-                      ),
                       ElevatedButton(
                         onPressed: () {
                           // Validate returns true if the form is valid, otherwise false.
                           if (_formKey.currentState.validate()) {
-                            // If the form is valid, display a snackbar. In the real world,
-                            // you'd often call a server or save the information in a database.
-
                             _formKey.currentState.save();
-
-                            ScaffoldMessenger
-                                .of(context)
-                                .showSnackBar(SnackBar(content: Text('Processing Data')));
-                            //print(_formKey.currentState);
 
                             setState(() {
                               var translated = Translator.translateToMorse(_textToTranslate, AlphabetITU());
                               print('Translated: $translated');
                               _textTranslated = translated;
+                              print(isSelected);
                             });
 
-                            if(isSelected[0]){
+                            HapticPlayer hapticPlayer = new HapticPlayer();
 
+                            if(isSelected[0]){
+                              print('Turning on light...');
+                              hapticPlayer.addPlayer(new HapticPlayerLightDecorator());
                             }
 
                             if(isSelected[1]){
-
+                              print('Turning on vibrations...');
+                              hapticPlayer.addPlayer(new HapticPlayerVibrateDecorator());
                             }
 
                             if(isSelected[2]){
+                              print('Turning on audio...');
+                              hapticPlayer.addPlayer(new HapticPlayerAudioDecorator());
+                            }
 
+                            //await compute(hapticPlayer.playText, _textTranslated);
+                            //compute(hapticPlayer.playText, _textTranslated);
+                            if(hapticPlayer.players.isNotEmpty){  //If there are players, play
+                              hapticPlayer.playText(_textTranslated);
                             }
                           }
                         },
@@ -105,18 +109,22 @@ class _TranslateToMorsePageState extends State<TranslateToMorsePage> {
               ),
           ),
         ),
+        Divider(thickness: 2.5),
         Expanded(
           flex: 5,
           child: Container(
             // this container won't be larger than
             // half of its parent size
-            child: Row(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                Text(
+                  'Translation',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
                 Text('$_textTranslated'),
                 TextButton.icon(
                   onPressed: () async {
-                    // Respond to button press
                     ClipboardData data = ClipboardData(text: '$_textTranslated');
                     await Clipboard.setData(data);
 
@@ -125,7 +133,7 @@ class _TranslateToMorsePageState extends State<TranslateToMorsePage> {
                         .showSnackBar(SnackBar(content: Text('Morse Code copied to clipboard')));
                   },
                   icon: Icon(Icons.sticky_note_2, size: 18),
-                  label: Text(""),
+                  label: Text("Copy to Clipboard"),
                 )
               ],
             )
