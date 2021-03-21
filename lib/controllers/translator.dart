e43018c237182c5b55ca91cb1760a036bc0b7806
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-03-21 12:44:57 +0100

    Translator +ErrorType +translateFromMorse

--- lib/controllers/translator.dart
+++ lib/controllers/translator.dart
@@ -2,6 +2,11 @@ import 'package:morse_code_translator/models/alphabet.dart';
 
 //https://en.wikipedia.org/wiki/Morse_code
 
+class TranslationError implements Exception {
+  String cause;
+  TranslationError(this.cause);
+}
+
 class Translator {
   //https://stackoverflow.com/questions/50431055/what-is-the-difference-between-the-const-and-final-keywords-in-dart
   static const _letter_gap = '   ';
@@ -12,12 +17,15 @@ class Translator {
 
     for(int i = 0; i < textToTranslate.length; i++){
       if(alphabet.containsCharTranslation(textToTranslate[i])){
-        morseString += alphabet.getCharTranslation(textToTranslate[i]) + _letter_gap;
+        try{
+          morseString += alphabet.getCharTranslation(textToTranslate[i]) + _letter_gap;
+        } on AlphabetError{
+          throw new TranslationError("No morse translation found for this character");  //If char isn't valid morse symbol
+        }
       } else if (textToTranslate[i] == ' ' || textToTranslate[i] == '.') {
         morseString += _word_gap;
       } else {
-        print("returning null...");
-        return null;
+        throw new TranslationError("No morse translation found for this character");  //If char isn't valid morse symbol
       }
     }
 
@@ -26,17 +34,45 @@ class Translator {
 
   static String translateFromMorse(String textToTranslate, Alphabet alphabet){
     String alphaNumericString = '';
-    for(int i = 0; i < textToTranslate.length; i++){
-      if(textToTranslate[i] == ' ' || textToTranslate[i] == '.'){
-        alphaNumericString += _word_gap;
+    for(int i = 0; i < textToTranslate.length;){
+      if(!(textToTranslate[i] == '.' || textToTranslate[i] =='-' || textToTranslate[i] ==' ')){
+        print(textToTranslate[i]);
+        throw new TranslationError("Invalid symbol found");  //If char isn't valid morse symbol
       }
-      else if(alphabet.alphabet.containsKey(textToTranslate[i].toLowerCase())){ //Better to have last? Most characters will NOT be punctuation,
-        //charToAdd = _alphabet_international_ITU[textToTranslate[i]];
-        alphaNumericString += alphabet.alphabet[textToTranslate[i].toLowerCase()] + _letter_gap;
+      if(textToTranslate[i] == ' '){
+        if(textToTranslate.substring(i, i + 2) == '   ' && textToTranslate[i + 3] != ' '){  //If is letter gap
+          i += 3;
+          continue;
+        }
+        else if(textToTranslate.substring(i, i + 6) == '   ' && textToTranslate[i + 7] != ' '){  //If is word gap
+          i += 7;
+          continue;
+        } else {  //Probably malformed
+          throw new TranslationError("Malformed space");  //If char isn't valid morse symbol
+        }
+      } else {
+        //Now we most likely have a valid morse character translation
+        String currentString = textToTranslate.substring(i);
+        String morseCharacter = currentString.split('   ')[0];  //The character ends at the next word gap i.e. three spaces / blanks
+        print(morseCharacter);
+
+        if(alphabet.containsMorseSymbolTranslation(morseCharacter)){
+          try{
+            alphaNumericString += alphabet.getMorseSymbolTranslation(morseCharacter);
+            i += morseCharacter.length;
+            continue;
+          } on AlphabetError {
+            throw new TranslationError("This morse block can't be properly translated, even though one should exist");
+          }
+        } else {
+          //https://stackoverflow.com/questions/13579982/how-to-create-a-custom-exception-and-handle-it-in-dart
+          //https://dart.dev/guides/language/language-tour#exceptions
+          //TODO stop returning null! Throw errors!
+          throw new TranslationError("This morse snippet doesn't seem to match any character translation");
+        }
       }
-      else {
-        return null;
-      }
+
+      i += 1;
     }
 
     return alphaNumericString;
