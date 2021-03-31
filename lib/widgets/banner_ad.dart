27e4fab0dd7a601117dab2ed35144ef33869897f
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-03-31 17:02:32 +0200

    BannerAdWidget size, alignment, & listener

--- lib/widgets/banner_ad.dart
+++ lib/widgets/banner_ad.dart
@@ -4,9 +4,22 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 class BannerAdWidget extends StatelessWidget{
   final BannerAd myBanner = BannerAd(
     adUnitId: 'ca-app-pub-3940256099942544/6300978111',
-    size: AdSize.getSmartBanner(Orientation),
+    size: AdSize.fullBanner,
     request: AdRequest(),
-    listener: AdListener(),
+    listener: AdListener(
+      // Called when an ad is successfully received.
+      onAdLoaded: (Ad ad) => print('Ad loaded.'),
+      // Called when an ad request failed.
+      onAdFailedToLoad: (Ad ad, LoadAdError error) {
+        print('Ad failed to load: $error');
+      },
+      // Called when an ad opens an overlay that covers the screen.
+      onAdOpened: (Ad ad) => print('Ad opened.'),
+      // Called when an ad removes an overlay that covers the screen.
+      onAdClosed: (Ad ad) => print('Ad closed.'),
+      // Called when an ad is in the process of leaving the application.
+      onApplicationExit: (Ad ad) => print('Left application.'),
+    ),
   );
 
   @override
@@ -16,7 +29,7 @@ class BannerAdWidget extends StatelessWidget{
     AdWidget adWidget = AdWidget(ad: myBanner);
     
     return Container(
-      alignment: Alignment.center,
+      alignment: Alignment.bottomCenter,
       child: adWidget,
       width: myBanner.size.width.toDouble(),
       height: myBanner.size.height.toDouble(),
