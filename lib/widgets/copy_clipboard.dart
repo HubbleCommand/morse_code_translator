d6d66bd35154ff09aef18f77e7425ae352cf7bc7
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-04-01 21:13:02 +0200

    CopyClipboardWidget clean

--- lib/widgets/copy_clipboard.dart
+++ lib/widgets/copy_clipboard.dart
@@ -1,39 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-/*
-Use like:
-CopyToClipboardWidget(
-                  onTapCopy: (){
-                    return _textTranslated;
-                  },
-                ),
- */
 class CopyToClipboardWidget extends StatelessWidget{
-  CopyToClipboardWidget({@required this.onTapCopy});
+  CopyToClipboardWidget({@required this.onTapCopy, @required this.message});
   final Function onTapCopy;
+  final String message;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        TextButton.icon(
-          onPressed: () async {
-            var text = onTapCopy();
-            // Respond to button press
-            print('Received text $text');
-            ClipboardData data = ClipboardData(text: '$text');
-            await Clipboard.setData(data);
+    return IconButton(
+        onPressed: () async {
+          var text = onTapCopy();
+          // Respond to button press
+          print('Received text $text');
+          ClipboardData data = ClipboardData(text: '$text');
+          await Clipboard.setData(data);
 
-            ScaffoldMessenger
-                .of(context)
-                .showSnackBar(SnackBar(content: Text('Morse Code copied to clipboard')));
-          },
-          icon: Icon(Icons.sticky_note_2, size: 18),
-          label: Text("Copy to Clipboard"),
-        )
-      ],
+          ScaffoldMessenger
+              .of(context)
+              .showSnackBar(SnackBar(content: Text(message)));
+        },
+        icon: Icon(Icons.sticky_note_2, size: 18),
     );
   }
 }
